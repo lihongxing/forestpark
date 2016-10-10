eb902cf5c297fff70d1ae0065a7a88047fc64f82
syjie <1211419561@qq.com>
2016-10-10 13:34:32 +0800

    数据库配置文件修改2016-10-10

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=forestpark',
+    'dsn' => 'mysql:host=192.168.163.102;dbname=forestpark',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
